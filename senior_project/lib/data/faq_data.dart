0ac1b542ced065bd7fbd5f840bd82a5fee223878
Omar Shaqaqi <shaqaqio@gmail.com>
2025-01-18 14:10:28 +0300

    making FAQ class and data

---
+++ senior_project/lib/data/faq_data.dart
@@ -0,0 +1,45 @@
+import '../models/faq.dart'; 
+
+final List<FAQ> faqData = [
+  FAQ(
+    question: "How to use FinWise?",
+    answer: "You can use FinWise by signing up and exploring its features.",
+  ),
+  FAQ(
+    question: "How much does it cost to use FinWise?",
+    answer: "FinWise is free to use with an option for premium features.",
+  ),
+  FAQ(
+    question: "How to contact support?",
+    answer: "You can contact support via the Help & Support section in the app.",
+  ),
+  FAQ(
+    question: "How can I reset my password if I forget it?",
+    answer:
+        "Use the 'Forgot Password' option on the login screen to reset your password.",
+  ),
+  FAQ(
+    question: "Are there any privacy or data security measures in place?",
+    answer:
+        "Yes, FinWise employs industry-standard security measures to protect your data.",
+  ),
+  FAQ(
+    question: "Can I customize settings within the application?",
+    answer: "Yes, you can customize your settings in the app settings section.",
+  ),
+  FAQ(
+    question: "How can I delete my account?",
+    answer:
+        "Go to the account settings section and select 'Delete Account'.",
+  ),
+  FAQ(
+    question: "How do I access my expense history?",
+    answer:
+        "Your expense history is available in the Transactions section.",
+  ),
+  FAQ(
+    question: "Can I use the app offline?",
+    answer:
+        "Yes, some features are available offline, but syncing requires internet.",
+  ),
+];
